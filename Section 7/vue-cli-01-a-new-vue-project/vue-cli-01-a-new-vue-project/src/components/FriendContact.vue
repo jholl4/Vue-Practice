021b2d3f8998d86692a2fbbd07693993ad562dcb
Joshua Holland <joshua.holland.1@outlook.com>
2024-05-14 14:37:39 -0400

    finished section 7

---
+++ Section 7/vue-cli-01-a-new-vue-project/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,36 @@
+<template lang="">
+    <div>
+        <li>
+            <h2>{{friend.name}}</h2>
+            <button @click="toggleDetails">{{detailsAreVisible ? 'Hide' : 'Show'}}</button>
+            <ul v-if="detailsAreVisible">
+                <li><strong>Phone: </strong> {{friend.phone}}</li>
+                <li><strong>Email: </strong> {{friend.email}}</li>
+            </ul>
+        </li>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            detailsAreVisible: false,
+            friend: {
+                id: 'manuel',
+                name: 'Manuel Lorenz',
+                phone: '555-555-5555',
+                email: 'manuel@localhost.com'
+            }
+        }
+    },
+    methods: {
+        toggleDetails() {
+            this.detailsAreVisible = !this.detailsAreVisible;
+        }
+    }
+    
+}
+</script>
+<style lang="">
+    
+</style>
