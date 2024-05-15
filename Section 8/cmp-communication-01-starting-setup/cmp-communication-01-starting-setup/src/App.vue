31830f258df3643e2a3762ed0dabae0c40567baa
Joshua Holland <joshua.holland.1@outlook.com>
2024-05-14 23:44:27 -0400

    more props and emits

--- Section 8/cmp-communication-01-starting-setup/cmp-communication-01-starting-setup/src/App.vue
+++ Section 8/cmp-communication-01-starting-setup/cmp-communication-01-starting-setup/src/App.vue
@@ -3,6 +3,7 @@
     <header>
       <h1>My Friends</h1>
     </header>
+    <new-friend key="friendForm" @add-friend-to-list="onAddFriendToList"></new-friend>
     <ul>
       <friend-contact v-for="friend in friends" :key="friend.id" :id="friend.id" :name="friend.name"
         :phone-number="friend.phone" :email-address="friend.email" :is-favorite="friend.isFavorite"
@@ -37,6 +38,11 @@ export default {
     toggleFavoriteStatus(friendId) {
       const identifiedFriend = this.friends.find(friend => friend.id === friendId);
       identifiedFriend.isFavorite = !identifiedFriend.isFavorite;
+    },
+    onAddFriendToList(newFriendToAdd) {
+      newFriendToAdd.id = new Date().toISOString();
+      newFriendToAdd.isFavorite = true;
+      this.friends.push(newFriendToAdd);
     }
   },
 };
@@ -73,7 +79,8 @@ header {
   list-style: none;
 }
 
-#app li {
+#app li,
+#app form {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
   border-radius: 10px;
@@ -105,5 +112,21 @@ header {
   background-color: #ec3169;
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+
+#app form div {
+  margin: 1rem 0;
 }
 </style>
---
+++ Section 8/cmp-communication-01-starting-setup/cmp-communication-01-starting-setup/src/components/NewFriend.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <form @submit.prevent="submitNewFriend">
+            <!-- <input type="text" placeholder="id" v-model="newFriend.id"> -->
+            <input type="text" placeholder="name" v-model="newFriend.name">
+            <input type="tel" placeholder="phone" v-model="newFriend.phone">
+            <input type="email" placeholder="email" v-model="newFriend.email">
+            <button @click.prevent="submitNewFriend">Add Contact</button>
+        </form>
+    </div>
+</template>
+<script>
+export default {
+    props: [],
+    emits: ['add-friend-to-list'],
+    data() {
+        return {
+            newFriend: {
+                // id: "",
+                name: "",
+                phone: "",
+                email: "",
+                // isFavorite: false,
+            }
+        }
+    },
+    methods: {
+        submitNewFriend() {
+            this.$emit('add-friend-to-list', this.newFriend)
+        }
+    }
+}
+</script>
+<style>
+#app input {
+    font: inherit;
+    padding: 0.15rem;
+}
+
+#app label {
+    font-weight: bold;
+    margin-right: 1rem;
+    width: 7rem;
+    display: inline-block;
+}
+
+#app form div {
+    margin: 1rem 0;
+}
+</style>
